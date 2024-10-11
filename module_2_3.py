510aad5be3055c5d5f4ef0cae1fed6c2ab43b707
Elijah887 <berez887@vk.com>
2024-10-11 15:55:29 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+z = 0
+while z < len(my_list):
+    print(my_list[0])
+    print(my_list[1])
+    print(my_list[2])
+    print(my_list[3])
+    print(my_list[5])
+    break
